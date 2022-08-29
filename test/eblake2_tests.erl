440cb91b7ae45fd8a9db55803237f24761a26ed1
Hans Svensson <hanssv@gmail.com>
2022-08-29 13:41:01 +0200

    Update enacl test dependency

--- test/eblake2_tests.erl
+++ test/eblake2_tests.erl
@@ -28,14 +28,14 @@ benchmark_test() ->
     %% Benchmark Blake2b against enacl.
     #{in := In, key := <<>>, out := Exp} = hd(filter_test_vectors(<<"blake2b">>)),
     HashLen = byte_size(Exp),
-    {T1, _} = timer:tc(fun() -> [ {ok, Exp} = enacl:generichash(HashLen, In) || _ <- lists:seq(1, 500) ] end),
+    {T1, _} = timer:tc(fun() -> [ Exp = enacl:generichash(HashLen, In) || _ <- lists:seq(1, 500) ] end),
     {T2, _} = timer:tc(fun() -> [ {ok, Exp} = eblake2:blake2b(HashLen, In) || _ <- lists:seq(1, 500) ] end),
 
     ?debugFmt("~.2f ms compared to ~.2f ms\n", [T1 / 1000, T2 / 1000]),
 
     BigData = <<0:(1024*10)>>,
 
-    {T3, _} = timer:tc(fun() -> [ {ok, _} = enacl:generichash(HashLen, BigData) || _ <- lists:seq(1, 50) ] end),
+    {T3, _} = timer:tc(fun() -> [ enacl:generichash(HashLen, BigData) || _ <- lists:seq(1, 50) ] end),
     {T4, _} = timer:tc(fun() -> [ {ok, _} = eblake2:blake2b(HashLen, BigData) || _ <- lists:seq(1, 50) ] end),
 
     ?debugFmt("~.2f ms compared to ~.2f ms\n", [T3 / 1000, T4 / 1000]),
@@ -49,7 +49,7 @@ random_test_() ->
 
 random_test(I) ->
     Data = crypto:strong_rand_bytes(I * 50),
-    {ok, Enacl}  = enacl:generichash(64, Data),
+    Enacl  = enacl:generichash(64, Data),
     {ok, Eblake} = eblake2:blake2b(64, Data),
 
     ?assertEqual(Eblake, Enacl).
